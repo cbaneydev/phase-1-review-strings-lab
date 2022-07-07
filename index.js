767b273b578372f66d2bc3819690ce682df1e153
Caeley Collins <cbaneydev@gmail.com>
2022-07-06 19:33:00 -0500

    finished project

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = "Billy"
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`
+
+const shortGreeting = `Welcome, ${currentUser[0]}`
+
+console.log(shortGreeting)
